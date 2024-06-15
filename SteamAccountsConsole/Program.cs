45e71d9e8e60cf3616da11b08acc91762a480e07
Robert <54269637+R-udren@users.noreply.github.com>
2024-06-15 18:28:47 +0300

    Add project files.

---
+++ SteamAccountsConsole/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using SteamAccountFinder;
+using Serilog;
+using System.Linq;
+
+namespace SteamAccountsConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Contains("debug"))
+			{
+				var loggerConfiguration = new LoggerConfiguration()
+			        .MinimumLevel.Debug()
+			        .WriteTo.Console()
+			        .WriteTo.File("log.txt");
+
+				SteamLibLogger.ConfigureLogger(loggerConfiguration);
+			}
+
+			PrintSteamAccounts();
+
+            SteamLibLogger.Shutdown();
+        }
+
+		private static void PrintSteamAccounts()
+		{
+
+			ulong activeSteamAccount = SteamAccounts.ActiveSteamAccount;
+            ulong runningGame = SteamAccounts.RunningGame;
+            var allAccounts = SteamAccounts.SteamUsers;
+            allAccounts.SortByLatestSource();
+			var steamIds = allAccounts.GetSteamIds();
+
+			Console.ForegroundColor = ConsoleColor.Red;
+            if (SteamAccounts.IsSteamRunning)
+            {
+                Console.Write("Active Steam Account: ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(activeSteamAccount);
+            }
+            else
+            {
+                Console.WriteLine("Steam is not running.");
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            if (SteamAccounts.IsGameRunning)
+            {
+                Console.Write("Running Game: ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(runningGame);
+            }
+            else
+            {
+                Console.WriteLine("Game is not running.");
+            }
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write($"\nFound {allAccounts.Count} Steam Accounts: ");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(string.Join(", ", steamIds));
+
+            foreach (var account in allAccounts)
+            {
+                if (account.Username != null)
+				{
+					Console.ForegroundColor = ConsoleColor.Cyan;
+					Console.Write("\nUsername: ");
+					Console.ForegroundColor = ConsoleColor.White;
+					Console.Write(account.Username);
+				}
+
+				Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("\nSteam ID: ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(account.SteamId);
+                if (account.IsActive)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(" - Active!");
+                }
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("\nProfile URL: ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(account.ProfileUrl);
+
+				Console.ForegroundColor = ConsoleColor.Cyan;
+				Console.Write("Sources Count: ");
+                if (account.SourcesCount > 2)
+				{
+					Console.ForegroundColor = ConsoleColor.White;
+				}
+				else
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+				}
+				Console.Write(account.SourcesCount);
+				if (account.LatestSource != DateTime.MinValue)
+                Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine(" (" + account.LatestSource.ToString("yyyy-MM-dd HH:mm:ss") + ")");
+
+			}
+
+            Console.ResetColor();
+            Console.Write("\nPress \"Enter\" key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SteamAccountsConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SteamAccountsConsole")]
+[assembly: AssemblyDescription("Console interface for libSteamFinder")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("InSoulGlobal")]
+[assembly: AssemblyProduct("SteamAccountsConsole")]
+[assembly: AssemblyCopyright("Copyright © InSoulGlobal 2024")]
+[assembly: AssemblyTrademark("InSoulGlobal")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d1f6eb79-7372-4773-9a07-80fa6f6faefa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.2.1")]
+[assembly: AssemblyFileVersion("1.0.2.1")]
